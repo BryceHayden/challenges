8d6b14816200ff614f68fab710b5a4070a361fc1
Bryce Hayden <bryce.c.hayden@gmail.com>
2019-03-18 20:39:11 -0600

    Switch promise example to use normal functions not es6

--- 01-javascript/06-promise/promise-example.js
+++ 01-javascript/06-promise/promise-example.js
@@ -1,12 +1,12 @@
 function callBackend(isLoggedIn) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
       if (isLoggedIn) {
         //Do the happy path here and return [{ name: 'john', age: 82 }, { name: 'Sammy', age: 18 }, { name: 'Tommy', age: 27 }]
         resolve([{ name: 'john', age: 82 }, { name: 'Sammy', age: 18 }, { name: 'Tommy', age: 27 }])
       } else {
         //Do the unhappy path here and return a messgae
-        reject('User is not logged in')
+        reject({msg: 'User is not logged in'})
       }
     }, 3000)
   });
@@ -15,18 +15,17 @@ function callBackend(isLoggedIn) {
 function main () {
   let users = []
 
-  const results = callBackend(true)
+  const results = callBackend(false)
   //Use the returned promise to get the happy path
 
   //Use the catch case for the unhappy path
   results
-    .then ((data) => {
+    .then (function (data) {
       users = data
       console.log("User", users)
     })
-    .catch((error) => {
+    .catch(function (error) {
         console.log('Oh no...', error.msg)
     })
 }
-
 main()
